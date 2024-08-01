a93d3e2599b2331cc5654a4f9504e736acc3a9cb
Tatiana Nunes da Silva Santana <167986988+tatcom23@users.noreply.github.com>
2024-08-01 09:17:18 -0300

    exercício contendo método construtor, encapsulamento, modificadores de acesso e properties

---
+++ Construtores/Construtores/ContaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtores
+{
+    class ContaBancaria
+    {
+        public string Nome;
+        public int Conta { get; private set; }
+        public double Saldo { get; private set; }
+
+        
+        public ContaBancaria(string nome, int conta)
+        {
+            Nome = nome;
+            Conta = conta;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + Conta
+                + ", Titular: " + Nome
+                + ", R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+         }
+    }
+}
--- Construtores/Construtores/Program.cs
+++ Construtores/Construtores/Program.cs
@@ -6,7 +6,39 @@ namespace Construtores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string titular;
+            int conta;
+            double quantia;
+            
+            Console.Write("Entre com o número da conta: ");
+            conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o nome do titular da conta: ");
+            titular = Console.ReadLine();
+
+            ContaBancaria Cliente = new ContaBancaria(titular, conta);
+
+            Console.Write("Entre com o valor do depósito inicial: ");
+            quantia = double.Parse(Console.ReadLine());
+            Cliente.Deposito(quantia);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(Cliente);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para depósito: ");
+            quantia = double.Parse(Console.ReadLine());
+            Cliente.Deposito(quantia);
+            Console.WriteLine("Dados atualizados: ");
+            Console.WriteLine(Cliente);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine());
+            Cliente.Saque(quantia);
+            Console.WriteLine("Dados atualizados: ");
+            Console.WriteLine(Cliente);
+           
         }
     }
 }
